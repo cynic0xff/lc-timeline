51bcc229374a768b2a72e23ff408f4b38e756003
Scott Wren <scott.wren@nrtsoutheast.com>
2019-06-05 13:44:19 -0400

    Move delivery date components into the stepper

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-stepper',
@@ -18,12 +18,8 @@ export class StepperComponent implements OnInit {
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
+    this.firstFormGroup = this._formBuilder.group({});
+    this.secondFormGroup = this._formBuilder.group({});
   }
 
 }
